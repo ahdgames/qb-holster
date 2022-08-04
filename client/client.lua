ff087310a6649e76c8d40a0d14caed3827588499
YoungDev <odjickfelix@gmail.com>
2022-08-04 07:34:25 -0400

    Added female and male variants

--- client/client.lua
+++ client/client.lua
@@ -3,6 +3,12 @@ local QBCore = exports['qb-core']:GetCoreObject()
 wearingHolster = false
 playerped = PlayerPedId()
 
+if IsPedMale then
+    male = true
+else
+    male = false
+end
+
 RegisterNetEvent("qb-holster:client:updateHolster", function()
     if not wearingHolster then
         text = "Putting on Holster"
@@ -10,7 +16,7 @@ RegisterNetEvent("qb-holster:client:updateHolster", function()
         text = "Removing Holster"
     end
 
-    QBCore.Functions.Progressbar('holster', text, Config.DrawTime, false, true, { -- Name | Label | Time | useWhileDead | canCancel
+    QBCore.Functions.Progressbar('holster', text, Config.HolsterTime, false, true, { -- Name | Label | Time | useWhileDead | canCancel
         disableMovement = false,
         disableCarMovement = true,
         disableMouse = false,
@@ -20,11 +26,19 @@ RegisterNetEvent("qb-holster:client:updateHolster", function()
         if not wearingHolster then
             wearingHolster = true
             TriggerEvent('weapons:ResetHolster')
-            SetPedComponentVariation(playerped, 7, Config.WearingNumber, 0, 2)
+            if male then
+                SetPedComponentVariation(playerped, 7, Config.MaleWearingNumber, 0, 2)
+            else
+                SetPedComponentVariation(playerped, 7, Config.FemaleWearingNumber, 0, 2)
+            end
         else
             wearingHolster = false
             TriggerEvent('weapons:ResetHolster')
-            SetPedComponentVariation(playerped, 7, Config.RemovedNumber, 0, 2)
+            if male then
+                SetPedComponentVariation(playerped, 7, Config.MaleRemovedNumber, 0, 2)
+            else
+                SetPedComponentVariation(playerped, 7, Config.FemaleRemovedNumber, 0, 2)
+            end
         end
 
     end, function() -- Play When Cancel
--- config.lua
+++ config.lua
@@ -1,5 +1,12 @@
+--These are default settings for a fresh server without EUP you may adjust them to your needs.
+
 Config = {}
 
-Config.DrawTime = 3000 -- Time in ms to draw weapon
-Config.WearingNumber = 17 -- Neck accessory that will appear once used
-Config.RemovedNumber = 0 -- Neck accessory that will appear once removed
+Config.HolsterTime = 3000 -- Time in ms to add/remove holster
+
+--Male options
+Config.MaleWearingNumber = 130 -- Neck accessory that will appear once used
+Config.MaleRemovedNumber = 0 -- Neck accessory that will appear once removed
+--Female options
+Config.FemaleWearingNumber = 160 -- Neck accessory that will appear once used
+Config.FemaleRemovedNumber = 0 -- Neck accessory that will appear once removed
